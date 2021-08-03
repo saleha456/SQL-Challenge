31b1c6195ff7abe0eec2115f988ede80328b53a8
Saleha <salehaahmed456@gmail.com>
2021-08-03 00:03:06 -0400

    last name frequency

--- EmployeeSQL/Analysis.sql
+++ EmployeeSQL/Analysis.sql
@@ -17,11 +17,36 @@ where date_part('year', hire_date) = 1986 ;
 --Manager of each department
 select dm.dept_no, d.dept_name, dm.emp_no, e.first_name, e.last_name from dept_manager dm
 left join departments d on dm.dept_no = d.dept_no
-left join employees e on dm.emp_no = e.emp_no
+left join employees e on dm.emp_no = e.emp_no;
 
 
---select * from employees
---where emp_title = "m0001"
+-- List of Department of each Employee
+select e.emp_no, e.last_name, e.first_name, d.dept_name from employees e
+inner join dept_emp de on e.emp_no = de.emp_no
+left join departments d on de.dept_no = d.dept_no;
 
---select * from titles
 
+--- People with First Name 'Hercules' and Last Name Starting with 'B'
+select e.first_name, e.last_name, e.sex from employees e
+where e.first_name = 'Hercules' and e.last_name like 'B%';
+
+
+---All Employees in Sales Department
+select de.emp_no, e.last_name, e.first_name, d.dept_name from dept_emp de
+left join departments d on de.dept_no = d.dept_no
+left join employees e on de.emp_no = e.emp_no
+where d.dept_name = 'Sales'
+ 
+ 
+---All Employees in Sales and Development Departments
+select de.emp_no, e.last_name, e.first_name, d.dept_name from dept_emp de
+left join departments d on de.dept_no = d.dept_no
+left join employees e on de.emp_no = e.emp_no
+where d.dept_name in ('Sales','Development');
+
+
+
+---Frequence of Employee Last Names
+select last_name, count(last_name) as "frequency" from employees
+group by last_name
+order by "frequency" desc;
